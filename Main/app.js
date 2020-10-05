cd6dd781b632253bf29aace0c2be3cbd9826996d
Saud Naumani <saud.naumani@shopify.com>
2020-10-04 22:59:02 -0400

    fixed changes to search bar

--- Main/app.js
+++ Main/app.js
@@ -1,4 +1,5 @@
 const searchBar = document.getElementById('pokiName');
+const searchBar1 = document.getElementById('Number');
 const unlist = document.getElementById("cards");
 //load in all tags "li" within cards
 const list = unlist.getElementsByTagName("li");
@@ -36,6 +37,39 @@ searchBar.addEventListener('keyup', (e) => {
         });
     });
 
+    searchBar1.addEventListener('keyup', (e) => { 
+        var charactersList = document.getElementById('charactersList');
+        charactersList.innerHTML = " ";
+        let filteredCharacters = []; 
+        const searchString = e.target.value.toLowerCase();
+        console.log (searchString);
+        if(searchString == ""){
+            myDiv.style.display = "none";
+        }
+        else{
+            myDiv.style.display = "block";
+        }
+       // console.log(list);
+        for(let i=0; i<20;i++){
+            //load h2 headers
+            const card_id = list[i].getElementsByClassName("card-title")[0];
+            if(card_id.innerHTML.toLowerCase().indexOf(searchString) > -1){ 
+                filteredCharacters.push(list[i]);
+              //  var cln = list[i].cloneNode(true);
+                }
+            }
+            console.log(filteredCharacters);
+            
+            filteredCharacters.forEach((character) => {
+                var cln = character.cloneNode(true);
+                charactersList.appendChild(cln);
+                
+            });
+        });
+
+
+
+    
 
 
     
